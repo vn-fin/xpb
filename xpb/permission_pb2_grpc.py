e9ef4255bd83366817cfbbc579dc6509a7d952ff
kim <kimnt93@gmail.com>
2025-11-27 10:54:08 +0700

    Add py package

--- xpb/permission_pb2_grpc.py
+++ xpb/permission_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import permission_pb2 as permission__pb2
+import xpb.permission_pb2 as permission__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
